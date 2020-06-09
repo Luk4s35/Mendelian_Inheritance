6c3fd59dbc7f10a2ec5f35dc440bc612c3032108
Bram Ratz <bratz@uoguelph.ca>
2020-06-09 17:01:26 -0400

    Updated code for project

--- simpleInheritance.py
+++ simpleInheritance.py
@@ -4,11 +4,19 @@
 Given three positive integers, k, m, n, representing a population 
 containing k+m+n individuals, where k = homozygous dominant, m = heterozygous,
 and n = homozygous recessive. Returns the probability that any two 
-mating individuals will display the each of the three possible geneotypes
-and the two possible phenotypes.
-
+mating individuals will display the probability of two randomly selected
+organisms will produce an offspring with a dominant allele (and by default
+the dominant phenotype).
 Created on Tue Jun  9 14:32:36 2020
 
 @author: bram
 """
 
+# function for the dominant probability
+def Dom(k: int, m: int, n: int) -> float:
+    """
+    given three integers for pop with k, n, m number of individuals 
+    returns the probability of two random individuals producing an 
+    offsring with the dominant allele
+    ""
+    
