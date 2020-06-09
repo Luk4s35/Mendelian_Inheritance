98f97eebe27802732a891337773f4e98f1a4b256
Bram Ratz <bratz@uoguelph.ca>
2020-06-09 14:37:54 -0400

    Program to calculate simple mendelian inhertitance. Returns probability of two random mates from a given population will produce offspring with one of three genotypes and one of two phenotypes.

---
+++ simpleInheritance.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Given three positive integers, k, m, n, representing a population 
+containing k+m+n individuals, where k = homozygous dominant, m = heterozygous,
+and n = homozygous recessive. Returns the probability that any two 
+mating individuals will display the each of the three possible geneotypes
+and the two possible phenotypes.
+
+Created on Tue Jun  9 14:32:36 2020
+
+@author: bram
+"""
+
